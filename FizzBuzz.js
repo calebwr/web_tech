0fd3e102ab39ceb0504ebaec23d1fd0737722a8e
calebwr <cawright@ucollege.edu>
2015-09-03 21:16:16 +0000

    Fixed issue, put var=i inside the for loop closes issue #1

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,6 +1,5 @@
 function fizzbuzz(){
-    var i = 1;
-    for(i = 1; i < 101; i++) {
+    for(var i = 1; i = 1; i < 101; i++) {
     if((i % 3 == 0) && (i % 5 == 0))
         console.log("FizzBuzz")
         
